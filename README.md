9434ed9d28e6d5698890f59e33d95aa5f51cbe0c
Mukhammadrizo Maribjonov <m.maribjonov@innopilis.university>
2024-06-26 19:30:30 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Path Planner
+`ros2 launch path_planner nav2.launch.py`
---
+++ build/path_planner/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/rizo/SoftOverlord100/Overlord100-PathPlanner/install/path_planner'
